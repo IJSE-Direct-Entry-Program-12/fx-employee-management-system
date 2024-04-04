b257a19f038331da7161605ab5e17a1a04df86c4
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-04-04 18:16:45 +0530

    Finish the ems

--- src/main/java/lk/ijse/dep12/ems/controller/MainViewController.java
+++ src/main/java/lk/ijse/dep12/ems/controller/MainViewController.java
@@ -1,5 +1,6 @@
 package lk.ijse.dep12.ems.controller;
 
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
@@ -10,6 +11,9 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import lk.ijse.dep12.ems.to.Employee;
+
+import java.io.*;
+import java.util.Optional;
 
 public class MainViewController {
 
@@ -26,6 +30,7 @@ public class MainViewController {
     public TextField txtName;
     public TextField txtNic;
     public HBox genderWrapper;
+    private final File DB_FILE = new File(System.getProperty("user.home"), ".ems.db");
 
     public void initialize() {
         /* Map columns to the domain object */
@@ -60,6 +65,55 @@ public class MainViewController {
         });
 
         container.setDisable(true);
+        loadEmployeeDetails();
+    }
+
+    private void loadEmployeeDetails(){
+        try {
+            if (!DB_FILE.exists()) {
+                DB_FILE.createNewFile();
+                return;
+            }
+
+            try (BufferedReader br = new BufferedReader(new FileReader(DB_FILE))) {
+                while (true) {
+                    String nic = br.readLine();
+                    if (nic == null) break;
+                    String name = br.readLine();
+                    String address = br.readLine();
+                    String gender = br.readLine();
+                    String newLine = br.readLine();
+
+                    if (name == null || address == null || gender == null || newLine == null ||
+                            !isValidNic(nic) || !isValidName(name) ||
+                            address.length() < 3 || !(gender.equals("MALE") || gender.equals("FEMALE")) ||
+                            !newLine.equals("")) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR,
+                                "Corrupted database found. Do you want to reinitialize the database?",
+                                ButtonType.YES, ButtonType.NO);
+                        alert.setHeaderText("Database Error");
+                        Optional<ButtonType> buttonType = alert.showAndWait();
+                        if (buttonType.get() == ButtonType.YES) {
+                            DB_FILE.delete();
+                            DB_FILE.createNewFile();
+                            return;
+                        } else {
+                            Platform.exit();
+                            return;
+                        }
+                    } else {
+                        ObservableList<Employee> employeeList = tblEmployee.getItems();
+                        employeeList.add(new Employee(nic, name, address, gender));
+                    }
+                }
+            }
+        }catch (IOException exception){
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Something went wrong. Try again. If the problem persists, contact DEP-12");
+            alert.setHeaderText("Loading Error");
+            alert.showAndWait();
+            exception.printStackTrace();
+            Platform.exit();
+        }
     }
 
     public void btnNewEmployeeOnAction(ActionEvent event) {
@@ -131,7 +185,7 @@ public class MainViewController {
             return;
         }
 
-        String nic = txtNic.getText().strip();
+        String nic = txtNic.getText().strip().toUpperCase();
         String name = txtName.getText().strip();
         String address = txtAddress.getText().strip();
         String gender = ((RadioButton) grpGender.getSelectedToggle()).getText();
@@ -148,14 +202,62 @@ public class MainViewController {
             }
         }
 
-        employeeList.add(new Employee(nic, name, address, gender));
-        btnNewEmployee.fire();
+        Employee employee = new Employee(nic, name, address, gender);
+        if (saveEmployee(employee)){
+            employeeList.add(employee);
+            btnNewEmployee.fire();
+        }else{
+            Alert alert = new Alert(Alert.AlertType.ERROR,
+                    "Failed to save the employee, something went wrong, try again.");
+            alert.setHeaderText("Save Failed");
+            alert.show();
+        }
+    }
+
+    private boolean saveEmployee(Employee employee){
+        // File dbFile = new File(System.getProperty("user.home"), ".ems.db");
+        try(var bw = new BufferedWriter(new FileWriter(DB_FILE, true))){
+            DB_FILE.createNewFile();
+            bw.write(employee.getNic() + "\n");
+            bw.write(employee.getName() + "\n");
+            bw.write(employee.getAddress() + "\n");
+            bw.write(employee.getGender() + "\n\n");
+            return true;
+        }catch (IOException e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public void btnDeleteOnAction(ActionEvent event) {
         ObservableList<Employee> employeeList = tblEmployee.getItems();
-        employeeList.remove(tblEmployee.getSelectionModel().getSelectedItem());
-        if (employeeList.isEmpty()) btnNewEmployee.fire();
+
+        if (deleteEmployee(tblEmployee.getSelectionModel().getSelectedItem())){
+            employeeList.remove(tblEmployee.getSelectionModel().getSelectedItem());
+            if (employeeList.isEmpty()) btnNewEmployee.fire();
+        }else{
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Failed to delete the employee, try again!");
+            alert.setHeaderText("Delete Failed");
+            alert.show();
+        }
+    }
+
+    private boolean deleteEmployee(Employee deleteEmployee){
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(DB_FILE))){
+            ObservableList<Employee> employeeList = tblEmployee.getItems();
+            for (Employee employee : employeeList) {
+                if (deleteEmployee == employee) continue;
+
+                bw.write(employee.getNic() + "\n");
+                bw.write(employee.getName() + "\n");
+                bw.write(employee.getAddress() + "\n");
+                bw.write(employee.getGender() + "\n\n");
+            }
+            return true;
+        }catch (IOException e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public void tblEmployeeOnKeyPressed(KeyEvent keyEvent) {
